cc430cb92143c61ab10f4538aa9c703e95521db2
Carlos de Alda <carlos@gmail.com>
2024-05-16 09:39:56 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.empresa.actividad019_ds_analysis {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.empresa.actividad019_ds_analysis to javafx.fxml;
+    exports com.empresa.actividad019_ds_analysis;
+}
